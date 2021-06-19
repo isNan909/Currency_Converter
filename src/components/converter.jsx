beaa46b3eb2760ce74fbeb10c33c3d64c11a0fd9
Ishan Manandhar <ishanmanandhar@lftechnology.com>
2021-06-19 18:29:38 +0545

    added conflict of same state into listbox ui

--- src/components/converter.jsx
+++ src/components/converter.jsx
@@ -96,111 +96,114 @@ function Converterapp() {
             </label>
             <input
               type="text"
-              value="two"
+              value=""
               className="w-full border-2 rounded-sm p-3"
               placeholder=""
             />
           </div>
-          <Listbox value={selectedOne} onChange={setSelectedOne}>
-            {({ open }) => (
-              <>
-                <Listbox.Label className="block text-sm font-medium text-gray-700">
-                  Assigned to
-                </Listbox.Label>
-                <div className="mt-1 relative">
-                  <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                    <span className="flex items-center">
-                      <img
-                        src={selectedOne.avatar}
-                        alt=""
-                        className="flex-shrink-0 h-6 w-6 rounded-full"
-                      />
-                      <span className="ml-3 block truncate">
-                        {selectedOne.name}
+          <div>
+            <Listbox value={selectedOne} onChange={setSelectedOne}>
+              {({ open }) => (
+                <>
+                  <Listbox.Label className="font-bold text-sm mb-3 block">
+                    From
+                  </Listbox.Label>
+                  <div className="mt-1 relative">
+                    <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                      <span className="flex items-center">
+                        <img
+                          src={selectedOne.avatar}
+                          alt=""
+                          className="flex-shrink-0 h-6 w-6 rounded-full"
+                        />
+                        <span className="ml-3 block truncate">
+                          {selectedOne.name}
+                        </span>
                       </span>
-                    </span>
-                    <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                      <SelectorIcon
-                        className="h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </Listbox.Button>
+                      <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                        <SelectorIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Listbox.Button>
 
-                  <Transition
-                    show={open}
-                    as={Fragment}
-                    leave="transition ease-in duration-100"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <Listbox.Options
-                      static
-                      className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
                     >
-                      {people.map((person) => (
-                        <Listbox.Option
-                          key={person.id}
-                          className={({ active }) =>
-                            classNames(
-                              active
-                                ? 'text-white bg-indigo-600'
-                                : 'text-gray-900',
-                              'cursor-default select-none relative py-2 pl-3 pr-9'
-                            )
-                          }
-                          value={person}
-                        >
-                          {({ selectedOne, active }) => (
-                            <>
-                              <div className="flex items-center">
-                                <img
-                                  src={person.avatar}
-                                  alt=""
-                                  className="flex-shrink-0 h-6 w-6 rounded-full"
-                                />
-                                <span
-                                  className={classNames(
-                                    selectedOne
-                                      ? 'font-semibold'
-                                      : 'font-normal',
-                                    'ml-3 block truncate'
-                                  )}
-                                >
-                                  {person.name}
-                                </span>
-                              </div>
-
-                              {selectedOne ? (
-                                <span
-                                  className={classNames(
-                                    active ? 'text-white' : 'text-indigo-600',
-                                    'absolute inset-y-0 right-0 flex items-center pr-4'
-                                  )}
-                                >
-                                  <CheckIcon
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
+                      <Listbox.Options
+                        static
+                        className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+                      >
+                        {people.map((person) => (
+                          <Listbox.Option
+                            key={person.id}
+                            className={({ active }) =>
+                              classNames(
+                                active
+                                  ? 'text-white bg-indigo-600'
+                                  : 'text-gray-900',
+                                'cursor-default select-none relative py-2 pl-3 pr-9'
+                              )
+                            }
+                            value={person}
+                          >
+                            {({ selectedOne, active }) => (
+                              <>
+                                <div className="flex items-center">
+                                  <img
+                                    src={person.avatar}
+                                    alt=""
+                                    className="flex-shrink-0 h-6 w-6 rounded-full"
                                   />
-                                </span>
-                              ) : null}
-                            </>
-                          )}
-                        </Listbox.Option>
-                      ))}
-                    </Listbox.Options>
-                  </Transition>
-                </div>
-              </>
-            )}
-          </Listbox>
-          <div>icon</div>
+                                  <span
+                                    className={classNames(
+                                      selectedOne
+                                        ? 'font-semibold'
+                                        : 'font-normal',
+                                      'ml-3 block truncate'
+                                    )}
+                                  >
+                                    {person.name}
+                                  </span>
+                                </div>
+
+                                {selectedOne ? (
+                                  <span
+                                    className={classNames(
+                                      active ? 'text-white' : 'text-indigo-600',
+                                      'absolute inset-y-0 right-0 flex items-center pr-4'
+                                    )}
+                                  >
+                                    <CheckIcon
+                                      className="h-5 w-5"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </>
+              )}
+            </Listbox>
+          </div>
+
+          <div>{/* icon */}</div>
           <div>
             <Listbox value={selectedTwo} onChange={setSelectedTwo}>
               {({ open }) => (
                 <>
-                  <Listbox.Label className="block text-sm font-medium text-gray-700">
-                    Assigned to
+                  <Listbox.Label className="block font-bold text-sm mb-3">
+                    To
                   </Listbox.Label>
                   <div className="mt-1 relative">
                     <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
