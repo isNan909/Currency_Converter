29899f6d6c98a030b0a54c84f4c2f10994cd7bb1
Ishan Manandhar <ishanmanandhar@lftechnology.com>
2021-06-19 15:17:08 +0545

    initial commit scafolding empty project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './stylesheets/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="max-w-4xl  m-auto">
+        <p className="text-green-400">
+          This is the react app working with Vite and Tailwind CSS.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
